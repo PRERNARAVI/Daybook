82ba66b2602c99c962a0640c29670c1697c63c12
pravi34 <prernaravi@gatech.edu>
2020-06-28 05:05:38 -0700

    Added more formatting

--- front-end/screens/JournalEntry.js
+++ front-end/screens/JournalEntry.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import {Text, TextInput, View, Block, StyleSheet} from 'react-native';
+import {Text, TextInput, View, Block, StyleSheet, Dimensions} from 'react-native';
+import {Button, theme } from 'galio-framework';
+const { height, width } = Dimensions.get('screen');
 
 export default class JournalEntry extends React.Component{
     render() {
+        const { navigation } = this.props;
+
         return (
             <View style={styles.background}>
             <View style={styles.prompt}>
-            <Text>Prompt Question here</Text>
+            <Text style={styles.textEntry}>What are you feeling upset about today? Why do you think you are feeling this way?</Text>
             </View>
             <View style={styles.textHere}>
             <TextInput placeholder={"Enter Jounal Entry Here..."}></TextInput>
             </View>
+            <Button
+                shadowless
+                style={styles.button}
+                color="#809fff"
+                fontFamily="Avenir Next"
+                onPress={() => navigation.navigate('Home')}>
+                Submit
+              </Button>
             
             </View>
         );
@@ -26,7 +38,7 @@ const styles = StyleSheet.create({
         height: 150,
         backgroundColor: '#bfcfff',
         borderRadius: 5,
-        padding: 30,
+        padding: 28,
         margin: 20
     },
     textHere: {
@@ -35,5 +47,19 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 30,
         margin: 20
+    },
+    button: {
+        width: width - theme.SIZES.BASE * 4,
+        height: theme.SIZES.BASE * 3,
+        shadowRadius: 0,
+        shadowOpacity: 0,
+        marginLeft: 31,
+    },
+    textEntry: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontFamily: "Avenir Next",
+        fontWeight: '600',
+        color: '#002080'
     }
 });
